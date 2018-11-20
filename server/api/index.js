519f7ffd1fdb8a3251faa80eb0fab95ffea35056
Danny <Lorddanp@aol.com>
2018-11-20 16:41:16 -0500

    Fix(trades api): fixes typo leading to server crash

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 module.exports = router;
 
 router.use('/users', require('./users'));
-router.use('/trades', require('/trades'));
+router.use('/trades', require('./trades'));
 router.use('/stocks', require('./stocks'));
 
 router.use((req, res, next) => {
