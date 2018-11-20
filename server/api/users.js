6bea857938d9fd52913367b9c1d8808a43d57cd1
Danny <Lorddanp@aol.com>
2018-11-20 16:56:03 -0500

    Fix(api): fixes order property in trades and stocks queries

--- server/api/users.js
+++ server/api/users.js
@@ -20,7 +20,7 @@ router.get('/:userId/stocks', async (req, res, next) => {
       where: {
         id: req.params.userId,
       },
-      order: ['stockName', 'asc'],
+      order: [['stockName', 'asc']],
     });
     res.json(stocks);
   } catch (err) {
@@ -34,7 +34,7 @@ router.get('/:userId/trades', async (req, res, next) => {
       where: {
         id: req.params.userId,
       },
-      order: ['createdAt', 'desc'],
+      order: [['createdAt', 'desc']],
     });
     res.json(trades);
   } catch (err) {
