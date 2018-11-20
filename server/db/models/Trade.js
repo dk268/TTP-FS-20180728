65e2fb2b3be6a37f6aebc28fbc953b4e20c3fc1a
Danny <Lorddanp@aol.com>
2018-11-20 12:46:49 -0500

    Models(Trade): adds virtual volumn for total amount to Trade model gcm "Models(Trade): adds virtual volumn for total amount to Trade model"

--- server/db/models/Trade.js
+++ server/db/models/Trade.js
@@ -2,7 +2,8 @@ const db = require('../db');
 const Sequelize = require('sequelize');
 
 const Trade = db.define('trade', {
-  tradeAmount: {
+  tradeCount: {
+    //number of stocks moved
     type: Sequelize.INTEGER,
   },
   tradeSymbol: {
@@ -12,7 +13,14 @@ const Trade = db.define('trade', {
     type: Sequelize.STRING,
   },
   tradePrice: {
+    //price per stock
     type: Sequelize.INTEGER,
+  },
+  tradeAmount: {
+    type: Sequelize.VIRTUAL,
+    get() {
+      return this.getDataValue('tradeCount') * this.getDataValue('tradePrice');
+    },
   },
 });
 
