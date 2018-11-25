4fed623a6226ca3d062da6488babd181f0360473
Danny <Lorddanp@aol.com>
2018-11-25 16:21:09 -0500

    API(trades): attempts to introduce IEX node API

--- server/api/trades.js
+++ server/api/trades.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
-const { Trade } = require('../db/models');
+const { Trade, Stock } = require('../db/models');
+import { IEXClient } from 'iex-api';
+import * as _fetch from 'isomorphic-fetch';
+
+const iex = new IEXClient(_fetch);
+iex.stockCompany('AAPL').then(company => console.log(company));
+
 module.exports = router;
 
 router.get('/', async (req, res, next) => {
@@ -23,6 +29,18 @@ router.get('/:tradeId', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     const newTrade = await Trade.create(req.body);
+    const [updatingStock, wasCreated] = await Stock.findOrCreate({
+      where: {
+        userId: newTrade.userId,
+        stockSymbol: newTrade.tradeSymbol,
+      },
+    });
+    if (wasCreated) {
+      const [, updatedStock] = await updatingStock.update({
+        stockName: newTrade.tradeName,
+        stockCount: newTrade.tradeCount,
+      });
+    }
     res.json(newTrade);
   } catch (err) {
     next(err);
