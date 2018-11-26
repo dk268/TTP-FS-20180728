9a83633e596932b0c711084eaa6c1e7dd87b9101
Danny <Lorddanp@aol.com>
2018-11-25 20:13:17 -0500

    Cleanup(api): removes extra console logs from trades.js routes file

--- server/api/trades.js
+++ server/api/trades.js
@@ -31,30 +31,25 @@ router.get('/:tradeId', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-    console.log('DATA VALUES');
-    console.log(req.body);
     const currentPrice = await iex.stockPrice(req.body.tradeSymbol);
     if (!currentPrice) {
       res.status(405).send(`No such stock symbol.`);
       return `Error: symbol missing`;
     }
     if (currentPrice != req.body.tradePrice) {
-      res.status(407).send(`Price has since updated. Please try again.`);
+      res.status(405).send(`Price has since updated. Please try again.`);
       return `Error: price out of date`;
     }
-    console.log('before NEW TRADE');
     const newTrade = await Trade.create({
       ...req.body,
       tradePrice: currentPrice * 100,
     });
-    console.log('before STOCK FIND OR CREATE');
     const [updatingStock, wasCreated] = await Stock.findOrCreate({
       where: {
         userId: newTrade.userId,
         stockSymbol: newTrade.tradeSymbol,
       },
     });
-    console.log('before IF WAS CREATED');
     if (wasCreated) {
       await updatingStock.update(
         {
