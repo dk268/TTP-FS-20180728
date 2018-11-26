562f590ade37b74f0567bb76f2040bd7709759d5
Danny <Lorddanp@aol.com>
2018-11-26 17:17:45 -0500

    Feat(Trades): scaffolds Trades view

--- client/components/Trades.jsx
+++ client/components/Trades.jsx
@@ -1,4 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { LOADED, LOADING, UNASKED } from '../store';
+import { getTrades } from '../store/trades';
+
+class Trades extends Component {
+  componentDidMount = async () => {
+    const { id } = this.props.currentUser;
+    await this.props.getTrades(id);
+  };
+}
+
+const mapStateToProps = state => ({
+  currentUser: state.user,
+  currentTrades: state.trades,
+});
+
+const mapDispatchToProps = { getTrades };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Trades);
---
+++ client/components/helpers/TradeCard.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const TradeCard = props => {
+  return <h1>hi</h1>;
+};
+
+export default TradeCard;
