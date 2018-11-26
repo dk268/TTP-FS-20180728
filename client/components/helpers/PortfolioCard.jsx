842da6aa539f066fb722f4736ac86e5bd50b3d23
Danny <Lorddanp@aol.com>
2018-11-26 02:14:08 -0500

    View(portfolio): sweeping improvements to visual style of portfolio view

--- client/components/helpers/PortfolioCard.jsx
+++ client/components/helpers/PortfolioCard.jsx
@@ -10,20 +10,32 @@ const PortfolioCard = props => {
   console.log(stock, color);
   console.log(stock, stock.stockName, stock.openPrice);
   return (
-    <div className="stocks-map" key={stock.id}>
-      <h3 className="stocks-map-h3">
-        Company {stock.stockName}{' '}
-        <h3 className={`stocks-map-inner-h3 ${color}`}>
-          "{stock.stockSymbol}"
-        </h3>
-      </h3>
-      <h4 className="stocks-map-h4">
-        Your {stock.stockCount} {stock.stockCount * 1 > 1 ? 'stocks' : 'stock'}{' '}
-        valued at{' '}
-        <h4 className={`stocks-map-inner-h3 ${color}`}>{stock.currentPrice}</h4>{' '}
-        with opening price ${stock.currentPrice} total ${stock.stockAmount /
-          100}
-      </h4>
+    <div className="stocks-map-master-div">
+      <div className="stocks-map" key={stock.id}>
+        <div className="stocks-map-div">
+          Company Name: {stock.stockName} <br />
+          <div className={`stocks-map-inner-div ${color}`}>
+            "{stock.stockSymbol}"
+          </div>
+        </div>
+        <div className="stocks-map-div">
+          <div className={`stocks-map-current-price-div ${color}`}>
+            Current Price: ${stock.currentPrice + ' '}
+          </div>{' '}
+          Stocks: {stock.stockCount} <br />
+          Opening Price: ${stock.openPrice}
+        </div>
+      </div>
+      <div className={`stocks-map-summary-div ${color}-summary`}>
+        <h2>Stock: {stock.stockSymbol}</h2>
+        <h2>
+          Total: ${stock.stockAmount % 100
+            ? stock.stockAmount % 10
+              ? stock.stockAmount / 100
+              : stock.stockAmount / 100 + '0'
+            : stock.stockAmount / 100 + '.00'}
+        </h2>
+      </div>
     </div>
   );
 };
